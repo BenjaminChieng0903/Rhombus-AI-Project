cbe259ddfff802d20d0cb2932500f77cc7b74ff8
Benjamin Qian <qianjingning0903@gmail.com>
2024-10-31 02:00:13 +1100

    typeconvert-api-trival-changes
    typeconvert-api-trival-changes

--- client/src/apis/type-convert/TypeConvert.ts
+++ client/src/apis/type-convert/TypeConvert.ts
@@ -1,23 +1,28 @@
 import axiosApi from './../../axios/app'
-const TypeConvertApi = async(file:File | null)=>{
+import { AxiosResponse } from 'axios';
+const TypeConvertApi = async(file:File | null):Promise<AxiosResponse | null>=>{
+
     if (!file) {
         alert("Please select a file first!");
-        return;
+        return null;
     }
     const formData = new FormData();
     formData.append('file', file);
 
-    await axiosApi.post('/dataprocessing/typeconvert/',formData, {
+    const response = await axiosApi.post('/dataprocessing/typeconvert/',formData, {
         headers: {
             'Content-Type': 'multipart/form-data'
         }
     }).then((res)=>{
         alert("upload successfully");
         console.log(res)
+        return res.data.data;
 
     }).catch((err)=>{
         alert("Oops, something went wrong, try again");
+        return null;
     })
+    return response;
 }
 
 export default TypeConvertApi;
